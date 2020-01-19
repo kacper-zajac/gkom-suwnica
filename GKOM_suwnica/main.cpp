d424cd2712ae2fe3635ddf24255c93fc7947d4a7
q <zkacper03@gmail.com>
2020-01-19 02:40:25 +0100

    minor changes

--- GKOM_suwnica/main.cpp
+++ GKOM_suwnica/main.cpp
@@ -26,58 +26,23 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+unsigned int SCR_WIDTH;
+unsigned int SCR_HEIGHT;
 
-// camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
 
-// timing
 float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
 
 int main()
 {
-	// glfw: initialize and configure
-	// ------------------------------
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-#endif
-
-	// glfw window creation
-	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Suwnica Model 3D", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
-
-	// tell GLFW to capture our mouse
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 
-	// inicjalizuj glew
-	GLenum err = glewInit();
 
-	// configure global opengl state
-
-
-	float vertices[] = {
+	float verticiesCube[] = {
 		// positions          // normals           // texture coords
 		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
 		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
@@ -121,7 +86,6 @@ int main()
 		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
 		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
 	};
-
 	float skyboxVertices[] = {
 		// positions          
 		-1.0f,  1.0f, -1.0f,
@@ -167,171 +131,78 @@ int main()
 		 1.0f, -1.0f,  1.0f
 	};
 
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	SCR_WIDTH = glfwGetVideoMode(glfwGetPrimaryMonitor())->width;
+	SCR_HEIGHT = glfwGetVideoMode(glfwGetPrimaryMonitor())->height;
 
-	unsigned int indices[]
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Suwnica Model 3D", glfwGetPrimaryMonitor(), NULL);
+	if (window == NULL)
 	{
-		0, 1, 2,
-		2, 3, 0,
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
-		4, 5, 6,
-		6, 7, 4,
+	GLenum err = glewInit();
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-		8, 9, 10,
-		10, 11, 8,
 
-		12, 13, 14,
-		14, 15, 12,
 
-		16, 17, 18,
-		18, 19, 16,
-
-		20, 21, 22,
-		22, 23, 20,
-	};
-
-	// world space positions of our cubes
-	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f,  0.0f,  0.0f),
-		glm::vec3(2.0f,  5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f,  3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),
-		glm::vec3(1.5f,  2.0f, -2.5f),
-		glm::vec3(1.5f,  0.2f, -1.5f),
-		glm::vec3(-1.3f,  1.0f, -1.5f)
-	};
-
-	glm::vec3 lightPos(-1.3f, 2.0f, 2.0f);
-
-	VertexArray va, vaLight, exampleOne, skyboxVAO;
-	VertexBuffer vb(vertices, sizeof(vertices));
+	VertexArray skyboxVA;
 	VertexBuffer skyboxVB(skyboxVertices, sizeof(skyboxVertices));
-	VertexBufferLayout layout, skyboxLayout;
+	VertexBufferLayout skyboxLayout;
 	skyboxLayout.Push<float>(3);
-	skyboxVAO.AddBuffer(skyboxVB, skyboxLayout);
-	layout.Push<float>(3);
-	layout.Push<float>(3);
-	layout.Push<float>(2);
-	va.AddBuffer(vb, layout);
-	//IndexBuffer ib(indices, 6);
-
-	exampleOne.AddBuffer(vb, layout);
-	Shader basicShader("shaders/first.shader");
-	basicShader.Bind();
-	basicShader.SetUniform1i("u_Texture", 1);
-	
-	exampleOne.Unbind();
-	basicShader.Unbind();
-	Shader lightShader("shaders/texture.shader");
-	lightShader.Bind();
-	lightShader.SetUniform3f("light.direction", 14.0f, -14.0f, 18.0f);
-	lightShader.SetUniform3f("lightPos", lightPos.x, lightPos.y, lightPos.z);
-	lightShader.SetUniform3f("objectColor", 1.0f, 0.5f, 0.31f);
-	lightShader.SetUniform3f("lightColor", 1.0f, 1.0f, 1.0f);
-	lightShader.SetUniform3f("material.ambient", 1.0f, 0.5f, 0.31f);
-	lightShader.SetUniform1f("material.shininess", 32.0f);
-	lightShader.SetUniform3f("light.ambient", 0.2f, 0.2f, 0.2f);
-	lightShader.SetUniform3f("light.diffuse", 0.5f, 0.5f, 0.5f);
-	lightShader.SetUniform3f("light.specular", 1.0f, 1.0f, 1.0f);
-
-	Texture texture("textures/example.png");
-	
-	Texture textureMap("textures/map.png");
-	
-	texture.Bind(1);
-	textureMap.Bind(2);
-	lightShader.SetUniform1i("material.specular", 2);
-	lightShader.SetUniform1i("material.diffuse", 1);
-
-	//texture.Unbind();
-	va.Unbind();
-	lightShader.Unbind();
-	Shader lampShader("shaders/lamp.shader");
-
-	vaLight.AddBuffer(vb, layout);
-	lampShader.Bind();
-		
-	vaLight.Unbind();
-	vb.Unbind();
-	lampShader.Unbind();
-
-	Renderer renderer;	
-	
+	skyboxVA.AddBuffer(skyboxVB, skyboxLayout);
 	Skybox skybox;
 	unsigned int x = skybox.loadCubemap();
 	skybox.Bind();
 	Shader skyboxShader("shaders/skybox.shader");
 	skyboxShader.SetUniform1i("skybox", 0);
-	
 	glEnable(GL_DEPTH_TEST);
+
+	// ImGui
 	ImGui::CreateContext();
 	ImGui_ImplGlfwGL3_Init(window, true);
 	ImGui::StyleColorsDark();
 	bool show_demo_window = true;
 	bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-	glm::vec3 f(0.0f, 0.0f, 0.0f);
-	glm::vec3 y(0.0f, 0.0f, 0.0f);
-	// render loop
-	// -----------
+	// ImGui
+	Renderer renderer;	
+
 	while (!glfwWindowShouldClose(window))
 	{
-
-		// per-frame time logic
-		// --------------------
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		// input
-		// -----
 		processInput(window);
 
 		renderer.Clear();
 
 		ImGui_ImplGlfwGL3_NewFrame();
-
-		lightShader.Bind();
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		lightShader.setMat4("projection", projection);
 		glm::mat4 view = camera.GetViewMatrix();
-		lightShader.setMat4("view", view);
 		glm::mat4 model = glm::mat4(1.0f);
-		lightShader.setMat4("model", model);
-		lightShader.SetUniform3f("viewPos", camera.Position.x, camera.Position.y, camera.Position.z);
-		renderer.Draw(va, lightShader, 36);
-		
-		glm::vec3 x(2.0f, 0.0f, 0.0f);
-		basicShader.Bind();
-		basicShader.setMat4("projection", projection);
-		basicShader.setMat4("view", view);
-		model = glm::translate(model, x);
-		basicShader.setMat4("model", model);
-		renderer.Draw(exampleOne, basicShader, 36);
-
-		lampShader.Bind();
-		lampShader.setMat4("projection", projection);
-		lampShader.setMat4("view", view);
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, lightPos);
-		model = glm::scale(model, glm::vec3(0.2f));
-		lampShader.setMat4("model", model);
-		renderer.Draw(vaLight, lampShader, 36);
-
 		glDepthFunc(GL_LEQUAL);
 		skyboxShader.Bind();
 		skyboxShader.setMat4("projection", projection);
 		view = glm::mat4(glm::mat3(camera.GetViewMatrix()));
 		skyboxShader.setMat4("view", view);
-		renderer.Draw(skyboxVAO, skyboxShader, 36);
+		renderer.Draw(skyboxVA, skyboxShader, 36);
 		glDepthFunc(GL_LESS);
 
-		{
-			ImGui::SliderFloat3("light", &f.x, -1.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-			ImGui::SliderFloat3("myPos", &y.x, -1.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+		/*{
+			//ImGui::SliderFloat3("light", &f.x, -1.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+			//ImGui::SliderFloat3("myPos", &y.x, -1.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
 			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
@@ -347,7 +218,7 @@ int main()
 			if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
 				f.z -= 0.0001f;
 			
-		}
+		}*/
 
 
 		ImGui::Render();
@@ -364,6 +235,7 @@ int main()
 	glfwTerminate();
 	return 0;
 }
+
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
